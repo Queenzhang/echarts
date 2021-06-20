4a1063115a0ff2108c9a3f5ffdc4033192bf0bd2
Queenzhang <queen_zhang1994@163.com>
2021-06-20 23:21:19 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <h1>echarts 图表</h1>
+    <div id="main" style="width:200px;height:200px">
+    </div>
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts';
+// import { BarChart } from "echarts/charts";
+console.log(echarts)
+export default {
+  name: "App",
+  components: {
+    // BarChart
+  },
+  data() {
+    return {
+      option: {
+        xAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        },
+        yAxis: {
+          type: "value"
+        },
+        series: [
+          {
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: "bar",
+            showBackground: true,
+            backgroundStyle: {
+              color: "rgba(180, 180, 180, 0.2)"
+            }
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    var myChart = echarts.init(document.getElementById("main"));
+    myChart.setOption({
+      title: {
+        text: "ECharts 入门示例"
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    });
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
